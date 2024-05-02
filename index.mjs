cae1ed75df3c3e9632023a3ae958bf5b1a9813c0
bernarDuarte <bernar.duarte@hellosaks.com>
2024-05-02 20:15:38 -0300

    feat: added s3 check

--- index.mjs
+++ index.mjs
@@ -1,17 +1,23 @@
 import {log} from './log.mjs';
+import { s3 } from '@aws-sdk/client-s3'
+
+const s3client = new s3({region: 'us-east-1'})
 
 export const handler = async (event) => {
-    log("Log de Execução :" + JSON.stringify(event))  
-  
-    const response = {
-      statusCode: 200,
-      body: `<html><body>Dados da req ${JSON.stringify(event)}</html>`,
-      headers: {
-        "content-type": "text/html"
-      }
-    };
-    
-
-    return response;
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3client.getObject({
+        Bucket,
+        Key
+    })
+     const mega_byte = 1024 * 1024
+     if(getObjectResult.ContentLength > 1* mega_byte){
+        log('Objeto Muito Grande')
+        return 'Objeto Muito Grande'
+     } else{
+      log('Objeto Muito Grande')
+      return 'Objeto Muito Grande'
+     }
   };
   // aws lambda update-function-code --function-name primeiraFunc --zip-file fileb://lambda.zip
