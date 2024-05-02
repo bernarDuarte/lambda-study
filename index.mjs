f7362e9996dad5c0785289c36c09c767f83a9894
bernarDuarte <bernar.duarte@hellosaks.com>
2024-05-02 19:55:46 -0300

    chore: added req

--- index.mjs
+++ index.mjs
@@ -1,11 +1,16 @@
 import {log} from './log.mjs';
 
 export const handler = async (event) => {
+    log("Log de Execução :" + JSON.stringify(event))  
+  
     const response = {
       statusCode: 200,
-      body: JSON.stringify(event),
+      body: `<html><body>Dados da req ${JSON.stringify(event)}</html>`,
+      headers: {
+        "content-type": "text/html"
+      }
     };
-    log("Log de Execução :" + JSON.stringify(event))
+    
 
     return response;
   };
